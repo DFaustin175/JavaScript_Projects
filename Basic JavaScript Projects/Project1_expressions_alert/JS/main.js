5628b03474689d70d18fa5d675b633c5c97aa6a1
DFaustin175 <deefaustin175@gmail.com>
2022-01-10 23:38:46 -0500

    Basic Javascript

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,23 @@
+document.write("Hello World");
+document.write("<br>");
+
+
+var A = "This is a string";
+
+var string = "This is an alert";
+window.alert(string);
+
+var joke = "And then I said \"Lawnmower I hardly know her!\"";
+var B = joke + A;
+document.write(B);
+
+var food = "the foods", best = "Pizza", prettygood = "Pasta", awful = "Kale";
+//Changing Font Color
+var food = food.fontcolor("blue");
+var best = best.fontcolor("blue");
+var prettygood = prettygood.fontcolor("blue");
+var awful = awful.fontcolor("blue");
+//Changing Font Color
+document.write(food, best, prettygood, awful);
+
+document.write(8+8);
