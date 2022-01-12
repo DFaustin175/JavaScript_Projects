d0548a1610009d2f3dd5dffabc66183cac96b609
DFaustin175 <deefaustin175@gmail.com>
2022-01-11 23:25:51 -0500

    Java

---
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -0,0 +1,13 @@
+function Ride_Function() {
+    var Height, Can_ride;
+    Height = document.getElementById("Height").value;
+    Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
+}
+
+function canVote() {
+    var Age, Can_vote;
+    Age = document.getElementById("Age").value;
+    Can_vote = (Age < 18) ? "You are not old enough":"You are old enough";
+    document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
+}
