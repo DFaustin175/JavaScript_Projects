2c69cb216ecdb5c0f391e7782171bb34000b33b8
DFaustin175 <deefaustin175@gmail.com>
2022-01-12 12:11:22 -0500

    Ternary Operators and Constructors

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -11,3 +11,42 @@ function canVote() {
     Can_vote = (Age < 18) ? "You are not old enough":"You are old enough";
     document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
 }
+
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+
+function myFunction() {
+    document.getElementById("Keywords_and_Constructors").innerHTML = "Erik drives a " + Erik.Vehicle_Color + " -colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
+}
+
+function Birth(Month, Day, Year) {
+    this.Birth_Month = Month;
+    this.Birth_Day = Day;
+    this.Birth_Year = Year;
+}
+
+var Jack = new Birth("Feb", "2nd", 2002);
+var Emily = new Birth("Jan", "30th", 1980);
+var Erik = new Birth("Dec", "18th", 1971);
+
+function birthFun() {
+    document.getElementById("New_and_This").innerHTML = "Jack's birth day is " + Jack.Birth_Month + " " + Jack.Birth_Day + " " + Jack.Birth_Year;
+}
+
+function countUp() {
+    document.getElementById("Nested_Function").innerHTML = Count();
+    function Count() {
+        var start = 9;
+        function Plus_one() {start += 1;}
+        Plus_one();
+        return start;
+    }
+}
